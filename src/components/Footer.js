e4ca1b551b7260173df210699c2a54abd5f3cc3d
Tristan Yang <yanggc888@163.com>
2019-06-12 14:18:26 +0000

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  background: #333;
+  color: #fff;
+`;
+export default function Footer() {
+  return <Wrapper>Footer</Wrapper>;
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  background: #333;
+  color: #fff;
+`;
+export default function Header() {
+  return <Wrapper>Header</Wrapper>;
+}
